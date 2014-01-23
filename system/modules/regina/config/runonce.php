3b04c0ad794b87b50790e12063afd6645536017f
btri <ben.trinks@haus-e.de>
2014-01-23 15:11:41 +0100

    runonce modified for Contao 3

--- system/modules/regina/config/runonce.php
+++ system/modules/regina/config/runonce.php
@@ -7,8 +7,15 @@ class ReginaJob extends Controller
     }
     public function run()
     {
-        rename(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/tinyRegina.php', TL_ROOT.'/system/config/tinyRegina.php');
-        rename(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/advimageRegina', TL_ROOT.'/plugins/tinyMCE/plugins/advimageRegina');
+        if (version_compare(VERSION, '3.0', '>=')) {
+            rename(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/tinyRegina3.php', TL_ROOT.'/system/config/tinyRegina.php');
+            rename(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/advimageRegina', TL_ROOT.'/assets/tinymce/plugins/advimageRegina');
+            unlink(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/tinyRegina2.php');
+        } else {
+            rename(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/tinyRegina2.php', TL_ROOT.'/system/config/tinyRegina.php');
+            rename(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/advimageRegina', TL_ROOT.'/plugins/tinyMCE/plugins/advimageRegina');
+            unlink(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/tinyRegina3.php');
+        }
         rmdir(TL_ROOT.'/system/modules/regina/tinyMCEPlugin');
     }
 }
