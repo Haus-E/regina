147d641a0c4091385589c2ac002784c950f28ea3
btri <ben.trinks@haus-e.de>
2014-01-24 12:44:32 +0100

    changes in installation of the tinymce plugin

--- system/modules/regina/config/runonce.php
+++ system/modules/regina/config/runonce.php
@@ -5,20 +5,40 @@ class ReginaJob extends Controller
     {
         parent::__construct();
     }
+
     public function run()
     {
         if (version_compare(VERSION, '3.0', '>=')) {
-            rename(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/tinyRegina3.php', TL_ROOT.'/system/config/tinyRegina.php');
-            rename(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/advimageRegina', TL_ROOT.'/assets/tinymce/plugins/advimageRegina');
-            unlink(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/tinyRegina2.php');
+            copy(TL_ROOT . '/system/modules/regina/tinyMCEPlugin/tinyRegina3.php', TL_ROOT . '/system/config/tinyRegina.php');
+            $this->rcopy(TL_ROOT . '/system/modules/regina/tinyMCEPlugin/advimageRegina', TL_ROOT . '/assets/tinymce/plugins/advimageRegina');
         } else {
-            rename(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/tinyRegina2.php', TL_ROOT.'/system/config/tinyRegina.php');
-            rename(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/advimageRegina', TL_ROOT.'/plugins/tinyMCE/plugins/advimageRegina');
-            unlink(TL_ROOT.'/system/modules/regina/tinyMCEPlugin/tinyRegina3.php');
+            copy(TL_ROOT . '/system/modules/regina/tinyMCEPlugin/tinyRegina2.php', TL_ROOT . '/system/config/tinyRegina.php');
+            $this->rcopy(TL_ROOT . '/system/modules/regina/tinyMCEPlugin/advimageRegina', TL_ROOT . '/plugins/tinyMCE/plugins/advimageRegina');
         }
-        rmdir(TL_ROOT.'/system/modules/regina/tinyMCEPlugin');
+    }
+
+    public function rcopy($src, $dst)
+    {
+        if (file_exists($dst)) $this->rrmdir($dst);
+        if (is_dir($src)) {
+            mkdir($dst);
+            $files = scandir($src);
+            foreach ($files as $file)
+                if ($file != "." && $file != "..") $this->rcopy("$src/$file", "$dst/$file");
+        } else if (file_exists($src)) copy($src, $dst);
+    }
+
+    public function rrmdir($dir)
+    {
+        if (is_dir($dir)) {
+            $files = scandir($dir);
+            foreach ($files as $file)
+                if ($file != "." && $file != "..") $this->rrmdir("$dir/$file");
+            rmdir($dir);
+        } else if (file_exists($dir)) unlink($dir);
     }
 }
+
 $objZZReginaJob = new ReginaJob();
 $objZZReginaJob->run();
 ?>
