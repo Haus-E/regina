eeccc1ca429f659c820a7ce51a118ff086d47df7
kocd <daniel.koch@haus-e.de>
2014-01-24 11:38:33 +0100

    ajustments in german translations

--- system/modules/regina/languages/de/tl_regina.php
+++ system/modules/regina/languages/de/tl_regina.php
@@ -1,41 +1,42 @@
 <?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
-$GLOBALS['TL_LANG']['tl_regina']['imageData'] = 'Bilddaten';
+$GLOBALS['TL_LANG']['tl_regina']['imageData'] = 'Grundkonfiguration Bildtyp';
 $GLOBALS['TL_LANG']['tl_regina']['textData'] = 'Textdaten';
 $GLOBALS['TL_LANG']['tl_regina']['extraImage'] = 'weitere Bilddaten';
 
 $GLOBALS['TL_LANG']['tl_regina']['title'] = array('Titel', 'Titel des Bildtyps');
-$GLOBALS['TL_LANG']['tl_regina']['alias'] = array('Alias', 'Alias des Bildtyps das in der URL übermittelt wird.');
+$GLOBALS['TL_LANG']['tl_regina']['alias'] = array('Alias', 'Alias des Bildtyps. Der alias wird als parameter in der URL übermittelt.');
 $GLOBALS['TL_LANG']['tl_regina']['width'] = array('Breite', 'Breite des Zielbildes.');
 $GLOBALS['TL_LANG']['tl_regina']['height'] = array('Höhe', 'Höhe des Zielbildes');
-$GLOBALS['TL_LANG']['tl_regina']['resize'] = array('Einpassen (Upscale)', 'Soll das Bild ggf. vergrößert werden?');
-$GLOBALS['TL_LANG']['tl_regina']['scaleImg'] = array('Anpassen (Downscale)', 'Soll das Bild ggf. verkleinert werden?');
-$GLOBALS['TL_LANG']['tl_regina']['quality'] = array('Qualität in %', 'Qualität des Bildes in % (für JPEG Format)');
-$GLOBALS['TL_LANG']['tl_regina']['position'] = array('Positionierung', 'Sollen die Bilder mittig zentriert werden?');
-$GLOBALS['TL_LANG']['tl_regina']['grayscale'] = array('Graustufe in %', 'Graustufe des Bildes in %');
-$GLOBALS['TL_LANG']['tl_regina']['transcolor'] = array('Transparenz Farbe', 'Farbe die als Transparente Farbe interpretiert wird. (für PNG  Format)');
+$GLOBALS['TL_LANG']['tl_regina']['resize'] = array('Einpassen (Upscale)', 'Bilder werden vergrößert, das Bild in Breite <b>und</b> Höhe kleiner ist als die Zielwerte.');
+$GLOBALS['TL_LANG']['tl_regina']['scaleImg'] = array('Automatische Bildberechnung', 'Das Bild wird um den Faktor des Pixelverhältins aus den Einstellungen automatisch berechnet. Beispiel: Ausgangsbild 400*300px, Faktor 2, Zielbild ist 200*150px.');
+$GLOBALS['TL_LANG']['tl_regina']['quality'] = array('Qualität in %', 'Qualität des Bildes in % (nur für JPEG Format)');
+$GLOBALS['TL_LANG']['tl_regina']['position'] = array('Mittig zentrieren', 'Das Bild wird soweit möglich horizontal und vertikal mittig im übergeordneten DOM Element positioniert. (JS Funktion)');
+$GLOBALS['TL_LANG']['tl_regina']['visibility'] = array('Sichtbarkeit', '100% opaque -> 0% unsichtbar.');
+$GLOBALS['TL_LANG']['tl_regina']['grayscaleActive'] = array('Graustufe aktivieren', 'Bilder werden in Graustufen dargestellt.');
+$GLOBALS['TL_LANG']['tl_regina']['transcolor'] = array('Transparente Farbe', 'Farbe die als Transparente Farbe interpretiert wird. (für PNG  Format). Bei Umwandlung z.B. in JPEG werden transparente Bereiche des PNG-Bildes mit dieser Farbe dargestellt.');
 $GLOBALS['TL_LANG']['tl_regina']['imgType'] = array('Bildformat', 'Format in dem das Bild ausgegeben wird.');
-$GLOBALS['TL_LANG']['tl_regina']['constraints'] = array('Einpassen ohne abschneiden des Bildes', 'Geben sie an welche Ausdehnung eines Bildes eingepasst werden soll. (Bild wird nicht beschnitten)');
-$GLOBALS['TL_LANG']['tl_regina']['slice'] = array('Einpassen mit abschneiden des Bildes', 'Geben sie an welche Ausdehnung eines Bildes eingepasst werden soll. (Bild wird beschnitten)');
+$GLOBALS['TL_LANG']['tl_regina']['constraints'] = array('Einpassen ohne abschneiden des Bildes', 'Das Bild wird entsprechend den unter Breite und Höhe angegebenen Werten eingepasst.');
+$GLOBALS['TL_LANG']['tl_regina']['slice'] = array('Einpassen mit abschneiden des Bildes', 'Geben sie an welche Ausdehnung eines Bildes eingepasst werden soll. (Bild wird entsprechend beschnitten)');
 $GLOBALS['TL_LANG']['tl_regina']['croping'] = array('Bild beschneiden', 'Standard Croping Funktion.');
 
-$GLOBALS['TL_LANG']['tl_regina']['textUseActive'] = array('Text einrechnen', 'Diese Option aktivieren wenn Text in das Bild reingeschrieben werden soll.');
-$GLOBALS['TL_LANG']['tl_regina']['fontSize'] = array('Schriftgröße', 'Größe des Textes in Pixeln');
-$GLOBALS['TL_LANG']['tl_regina']['fontColor'] = array('Schriftfarbe', 'Schrift farbe in Hexadezimaler angabe');
-$GLOBALS['TL_LANG']['tl_regina']['fontNormal'] = array('Schirftart normaler Text', 'Schriftart für normalen Text auswählen.');
-$GLOBALS['TL_LANG']['tl_regina']['fontBold'] = array('Schrfitart fetter Text', 'Schriftart für fetten Text auswählen');
-$GLOBALS['TL_LANG']['tl_regina']['textBoxOffset'] = array('Position der Textes', 'Positionierung des Textes im Bild. (Oben, Rechts, Unten, Links - "-1" wird ignoriert)');
+$GLOBALS['TL_LANG']['tl_regina']['textUseActive'] = array('Text einrechnen', 'Diese Option aktivieren wenn Text in das Bild eingerechnet werden soll.');
+$GLOBALS['TL_LANG']['tl_regina']['fontSize'] = array('Schriftgröße', 'Größe des Textes in Punkt');
+$GLOBALS['TL_LANG']['tl_regina']['fontColor'] = array('Schriftfarbe', 'Schriftfarbe in hexadezimaler Schreibweise');
+$GLOBALS['TL_LANG']['tl_regina']['fontNormal'] = array('Schriftart normaler Text', 'Schriftart für normalen Text auswählen.');
+$GLOBALS['TL_LANG']['tl_regina']['fontBold'] = array('Schriftart fetter Text', 'Schriftart für fetten Text auswählen');
+$GLOBALS['TL_LANG']['tl_regina']['textBoxOffset'] = array('Position der Textes', 'Positionierung des Textes im Bild. (Oben, Rechts, Unten, Links - "-1" Angabe wird ignoriert)');
 $GLOBALS['TL_LANG']['tl_regina']['textAlign'] = array('Textausrichtung', 'Ausrichtung des Textes');
-$GLOBALS['TL_LANG']['tl_regina']['textCase'] = array('Texttransformation', 'Transforationen die auf den Text angewendet werden sollen.');
+$GLOBALS['TL_LANG']['tl_regina']['textCase'] = array('Texttransformation', 'Transformationen die auf den Text angewendet werden sollen.');
 
 $GLOBALS['TL_LANG']['tl_regina']['textBoxUse'] = array('Textbox verwenden', 'Soll der Text in einer Box erscheinen');
 $GLOBALS['TL_LANG']['tl_regina']['textPadding'] = array('Textabstand', 'Abstand des Textes zum Rand der Box.');
 $GLOBALS['TL_LANG']['tl_regina']['textBoxBGColor'] = array('Hintergrundfarbe der Textbox', 'Hexadezimaler Farbcode mit dem die Textbox gefüllt wird.');
-$GLOBALS['TL_LANG']['tl_regina']['textBoxHeight'] = array('Höhe der Textbox', 'Höhe für die Textbox in der der Text eingepasst wird');
-$GLOBALS['TL_LANG']['tl_regina']['textTransparency'] = array('Texttransparenz in %', 'Prozentuale Angabe wie Stark der Text die Textbox durchbrechen soll.');
-$GLOBALS['TL_LANG']['tl_regina']['textFactor'] = array('Vergrößerungsfaktor', 'Wenn Texttransparenz verwendet wird können unschöne Kanten entstehen. Der Textfaktor versucht deshalb über eine Vergrößerung des Textes Antialising zu simulieren.');
+$GLOBALS['TL_LANG']['tl_regina']['textBoxHeight'] = array('Höhe der Textbox', 'Höhe für die Textbox in der der Text eingepasst wird in Pixel');
+$GLOBALS['TL_LANG']['tl_regina']['textTransparency'] = array('Texttransparenz in %', 'Prozentuale Angabe wie stark der Text die Textbox durchbrechen soll.');
+$GLOBALS['TL_LANG']['tl_regina']['textFactor'] = array('Antialiasfaktor', 'Wenn Texttransparenz verwendet wird können unschöne Kanten entstehen. Dieser Faktor versucht deshalb über eine Vergrößerung des Textes Antialising zu simulieren.');
 
 $GLOBALS['TL_LANG']['tl_regina']['useExtraImage'] = array('Weitere Bilder einrechnen', 'Wählen sie diese Option wenn sie weitere Bilder in die Ausgabe einrechnen lassen wollen. (etwa ein Wasserzeichen)');
-$GLOBALS['TL_LANG']['tl_regina']['addImage'] = array(' ', 'Wählen sie hier die Bilder und ihre Positionierung aus.');
+$GLOBALS['TL_LANG']['tl_regina']['addImage'] = array(' ', 'Wählen sie hier die Bilder und ihre Positionierung aus. Relativer Pfad ab <b>root</b>, z.B. /tl_files/images/image.png. Positionierung oben, rechts, unten, links.');
 $GLOBALS['TL_LANG']['tl_regina']['addImageFile'] = array('Bilddatei', 'Wählen sie die Bilddatei die reingerechnet werden soll. (Der komplette Pfad ab dem Contaoverzeichnis muss angegeben werden)');
 $GLOBALS['TL_LANG']['tl_regina']['addImageOffset'] = array('Positionierung', 'Geben sie hier die Positionierrung an. (Oben, Rechts, Unten, Links - "-1" wird ignoriert)');
 
--- system/modules/regina/languages/de/tl_regina_settings.php
+++ system/modules/regina/languages/de/tl_regina_settings.php
@@ -5,7 +5,7 @@ $GLOBALS['TL_LANG']['tl_regina_settings']['regina'] = array(
     'useRegina' => array('Regina aktivieren', 'Mit setzen dieser Option werden alle Bilder für HD- / Mobile-Displays optimiert und per LazyLoad nachgeladen.'),
     'noOldIERegina' => array('Alte IEs ohne LazyLoad', 'Mit setzen dieser Option wird LazyLoad für IE Versionen <= 8 deaktiviert.'),
     'prefixRegina' => array('Trigger für RewriteRule', 'Setzen Sie einen eindeutigen Trigger für die RewriteRule z.B. <i>bild</i>'),
-    'useCacheRegina' => array('Bilder-Cache aktivieren', 'Wenn diese Option aktiv ist werden die Bilder zwischen gespeichert.'),
+    'useCacheRegina' => array('Bilder-Cache aktivieren', 'Wenn diese Option aktiv ist werden die Bilder zwischengespeichert.'),
     'cacheDirRegina' => array('Bilder-Cache Verzeichnis', 'Geben Sie den Ordner an in den die berechneten Bilder gecached werden sollen. (Es wird versucht den Ordner anzulegen)'),
 
     'rewriteRuleRegina' => array(
@@ -13,8 +13,8 @@ $GLOBALS['TL_LANG']['tl_regina_settings']['regina'] = array(
         'desc' => 'RewriteRule ^'.$GLOBALS['TL_CONFIG']['prefixRegina'].'/([\%\]\[A-Za-z0-9\-_]*)/(.*)$ system/modules/regina/img/img.php?plain=true&type=$1&file=$2 [qsa,L]'
     ),
 
-    'hdRatioRegina' => array('Pixelverhältins für HD Geräte', 'Geben sie an wie das Verhältnis der Pixel bei Geräten mit Hochauflösenden Displays sein soll'),
-    'mobiRatioRegina' => array('Pixelverhältins für mobile Geräte', 'Geben sie an wie das Verhältnis der Pixel bei mobilen Geräten sein soll'),
+    'hdRatioRegina' => array('Pixelverhältins für HD Geräte', 'Geben sie an wie das Verhältnis der Pixel bei Geräten mit Hochauflösenden Displays sein soll (Standard = 2)'),
+    'mobiRatioRegina' => array('Pixelverhältins für mobile Geräte', 'Geben sie an wie das Verhältnis der Pixel bei mobilen Geräten sein soll (Standard = 1.5)'),
 
     'defaultSettingsRegina' => array(
         'title' => 'Standardwerte',
@@ -28,11 +28,12 @@ $GLOBALS['TL_LANG']['tl_regina_settings']['regina'] = array(
     'explainRegina' => array(
         'title' => 'Erläuterungen',
         'desc' => 
-          "<p>Mit dem setzen dieser Option wird eine Erweiterte Ausgabe für HD und Mobile-Displays aktiviert. Beachten Sie dabei bitte folgende Punkte: </p>"
+          "<p>Mit dem setzen dieser Option wird eine erweiterte Ausgabe für HD und Mobile-Displays aktiviert. Beachten Sie dabei bitte folgende Punkte: </p>"
         . "<ul>"
             . "<li>Geben Sie unten einen Trigger an und speichern Sie die Seite.</li>"
             . "<li>Daraufhin erscheint eine RewriteRule die genau so als <b>eine</b> Zeile in die .htaccess Datei in das [root] Verzeichnis der Contaoinstallation kopiert werden muss.</li>"
-            . "<li>Damit Sie mit hochauflösenden Bildern arbeiten können müssen sämtlich Bilder egal ob Layout oder Inhalte in mindestens der doppelten Auflösung auf dem Server gespeichert werden. Das bedeutet: wollen Sie ein Bild in 350x150px anzeigen, sollte die Auflösung des Bildes mindesten 700x300px betragen.</li>"
+            . "<li>RewriteRule muss dafür zwingend aktivert werden. Der Server muss mod_rewrite unterstützen.</li>"
+            . "<li>Damit Sie mit hochauflösenden Bildern arbeiten können, müssen sämtliche Bilder egal ob Layout oder Inhalte in mindestens der doppelten Auflösung auf dem Server gespeichert werden. Das bedeutet: wollen Sie ein Bild in 350x150px anzeigen, sollte die Auflösung des Bildes mindesten 700x300px betragen.</li>"
         . "</ul>"
     )
 );
